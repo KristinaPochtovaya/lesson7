217c9f19c4df768b7f1f7d314295ae6737361221
Kris220019 <pochtovaya.kristina@mail.ru>
2020-06-25 18:47:14 +0300

    Lesson7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* В рамках задачи вам нужно реализовать компонеты Toggle, ToggleOn, ToggleOff, ToggleCheckbox так, 
+  чтобы заработал пример в App:
+  
+  * всегда виден checkbox, 
+  * если он включен - видны дети компонента ToggleOn
+  * если он выключен - видны дети компонента ToggleOff
+
+  "Живой" пример работы https://react-qv6ine.stackblitz.io/
+*/
+
+const Switch = ({ value, onClick }) => (
+  <input type="checkbox" value={value} onClick={onClick} />
+);
+
+class Toggle extends React.Component {
+  state = {
+    on: false,
+    activeItemIndex: 0,
+  };
+
+  toggle = () => this.setState({ on: !this.state.on });
+
+  render() {
+    return (
+      <>
+        <div>
+          {React.Children.map(this.props.children, (element, index) =>
+            React.cloneElement(
+              element,
+
+              {
+                on: this.state.on,
+                toggle: () => {
+                  this.setState({
+                    on: !this.state.on,
+                  });
+                },
+              }
+            )
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
+const ToggleOn = ({ on, children }) => <p>{on ? children : on}</p>;
+
+const ToggleOff = ({ on, children }) => <p>{on ? null : children}</p>;
+
+const ToggleCheckbox = ({ on, toggle }) => {
+  return <Switch value={on} onClick={toggle} />;
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <Toggle>
+        <ToggleOff>Видим, когда выключен</ToggleOff>
+        <ToggleCheckbox />
+        <ToggleOn>Видим, когда включен</ToggleOn>
+      </Toggle>
+    );
+  }
+}
 export default App;
